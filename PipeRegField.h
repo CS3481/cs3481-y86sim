a6b7d0e171e6be567f0dacefea0fe2c6235e7600
Eli Orians <eli.orians@gmail.com>
2021-11-30 17:51:48 -0500

    lab11 setup

--- PipeRegField.h
+++ PipeRegField.h
@@ -7,7 +7,7 @@ class PipeRegField
       uint64_t state;   //current state (output)
    public:
       //default state is 0 if none is provided
-      PipeRegField(int state = 0); 
+      PipeRegField(int state = 0);
       void setInput(uint64_t input);
       uint64_t getOutput();
       void normal();
