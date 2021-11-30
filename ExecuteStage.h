afcbee419892e67f1dc3874e6abce4c7ecea2bb4
Eli Orians <eli.orians@gmail.com>
2021-11-29 19:22:46 -0500

    oops

--- ExecuteStage.h
+++ ExecuteStage.h
@@ -11,6 +11,7 @@ class ExecuteStage : public Stage
         void setDstE(E * ereg, uint64_t & dstE, uint64_t e_icode, uint64_t e_Cnd);
         void setConditionCodes(uint64_t valE, uint64_t aluA, uint64_t aluB, uint64_t alufun);
         uint64_t ALU(uint64_t alufun, uint64_t aluA, uint64_t aluB);
+        uint64_t cond(uint64_t icode, uint64_t ifun);
 
     public:
         bool doClockLow(PipeReg ** pregs, Stage ** stages);
